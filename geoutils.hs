ac1e6538a4b48821ae748ff9f1551bad208667c8
JulienLeonard <jl.julien.leonard@gmail.com>
2015-07-29 14:49:34 +1000

    Add circle utils

--- geoutils.hs
+++ geoutils.hs
@@ -32,6 +32,8 @@ cradius (Circle _ r) = r
 ccenter :: Circle -> Point
 ccenter (Circle center _) = center
 
+cintersects :: Circle -> Circle -> Bool
+cintersects (Circle c1 r1) (Circle c2 r2) = vdist(vector c1 c2) < (r1 + r2)
 
 -- tests
 
@@ -40,3 +42,4 @@ test_vnorm = vnorm (Vector 1.0 1.0)
 test_cradius = cradius (Circle (Point 0.0 0.0) 1.5)
 test_ccenter = ccenter (Circle (Point 0.0 0.1) 1.5)
 test_vector = vector (Point 1.0 2.0) (Point 3.0 5.0)
+test_cintersects = cintersects (Circle (Point 0.0 0.0) 1.0) (Circle (Point 2.0 2.0) 1.0)
