1d25ed194a4358a2670eae588c8318430eaa6570
JulienLeonard <jl.julien.leonard@gmail.com>
2015-07-30 11:17:35 +1000

    Refactor code to create test utils and main files

--- geoutils.hs
+++ geoutils.hs
@@ -1,3 +1,5 @@
+module Geoutils where
+
 -- Geometry types
 
 data Point  = Point Float Float deriving (Show)
@@ -53,13 +55,3 @@ cintersects (Circle c1 r1) (Circle c2 r2) = vdist(vector c1 c2) < (r1 + r2)
 adjcircle :: Circle -> Float -> Float -> Circle
 adjcircle (Circle c1 r1) r a = Circle (padd c1 (vrotate (Vector 0.0 (r1+r)) a)) r
 
--- tests
-
-test_padd = padd (Point 1.0 2.0) (Vector 3.0 4.0)
-test_vnorm = vnorm (Vector 1.0 1.0)
-test_cradius = cradius (Circle (Point 0.0 0.0) 1.5)
-test_ccenter = ccenter (Circle (Point 0.0 0.1) 1.5)
-test_vector = vector (Point 1.0 2.0) (Point 3.0 5.0)
-test_cintersects = cintersects (Circle (Point 0.0 0.0) 1.0) (Circle (Point 2.0 2.0) 1.0)
-test_vrotate = vrotate (Vector 0.0 1.0) (3.14159/2.0)
-test_adjcircle = adjcircle (Circle (Point 0.0 0.0) 1.0) 2.0 (3.14159/2.0)
---
+++ test_geoutils.hs
@@ -0,0 +1,12 @@
+module Test_Geoutils where
+
+import Geoutils
+
+test_padd = padd (Point 1.0 2.0) (Vector 3.0 4.0)
+test_vnorm = vnorm (Vector 1.0 1.0)
+test_cradius = cradius (Circle (Point 0.0 0.0) 1.5)
+test_ccenter = ccenter (Circle (Point 0.0 0.1) 1.5)
+test_vector = vector (Point 1.0 2.0) (Point 3.0 5.0)
+test_cintersects = cintersects (Circle (Point 0.0 0.0) 1.0) (Circle (Point 2.0 2.0) 1.0)
+test_vrotate = vrotate (Vector 0.0 1.0) (3.14159/2.0)
+test_adjcircle = adjcircle (Circle (Point 0.0 0.0) 1.0) 2.0 (3.14159/2.0)
