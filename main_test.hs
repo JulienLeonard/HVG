82ded7edbd612d258a350a1305eab311c4d555c0
JulienLeonard <jl.julien.leonard@gmail.com>
2015-07-30 11:54:58 +1000

    Create specific main test

---
+++ main_test.hs
@@ -0,0 +1,13 @@
+import Geoutils
+import Test_Geoutils
+
+main = do 
+  print test_padd
+  print test_vnorm
+  print test_cradius
+  print test_ccenter
+  print test_vector
+  print test_cintersects
+  print test_vrotate
+  print test_adjcircle
+
