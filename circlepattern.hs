0aa540196f7eb336cf0859b5c1170072dd39f339
JulienLeonard <jl.julien.leonard@gmail.com>
2015-07-31 20:17:03 +1000

    Add first type of circlepattern as circlestring

--- circlepattern.hs
+++ circlepattern.hs
@@ -3,6 +3,11 @@ module CirclePattern where
 import Geoutils
 
 --- adj circle given radius and angle
-nextadjcircle :: Circle -> Float -> Float -> Circle
-nextadjcircle (Circle c1 r1) r a = Circle (padd c1 (vrotate (Vector 0.0 (r1+r)) a)) r
+nextadjcircle :: Circle -> (Float,Float) -> Circle
+nextadjcircle (Circle c1 r1) (r,a) = Circle (padd c1 (vrotate (Vector 0.0 (r1+r)) a)) r
 
+--- generate a list of circles from a list of radius
+--- TODO: refactor
+circlestring :: [Circle] -> Circle -> [(Float,Float)] -> [Circle]
+circlestring _ c [] = [c]
+circlestring cs c ((r,a):ras)  = cs ++ [c] ++ (circlestring [] (nextadjcircle c (r,a)) ras)
---
+++ test_circlepattern.hs
@@ -0,0 +1,6 @@
+module Test_CirclePattern where
+
+import CirclePattern
+import Geoutils
+
+test_circlepattern = circlestring [] (Circle (Point 0.0 0.0) 1.0) [(1.0,0.0),(1.0,0.0),(1.0,0.0)]
