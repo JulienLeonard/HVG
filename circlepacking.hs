004cdfe02bcfd813d1b432feba32f37e81aad983
JulienLeonard <jl.julien.leonard@gmail.com>
2015-08-05 10:00:12 +1000

    First implementation of circlepacking

--- circlepacking.hs
+++ circlepacking.hs
@@ -2,10 +2,22 @@ module CirclePacking where
 
 import Geoutils
 
+data Seed = Seed (Circle,Circle) Float deriving (Show)
+
+--- seed factory
+circlepairs2seeds :: [(Circle,Circle)] -> [Float] -> [Seed]
+circlepairs2seeds pairs sides = [Seed pair side | pair <- pairs, side <- sides]
+
+circlepair0 = ((Circle (Point 0.0 0.0) 1.0),(Circle (Point 2.0 0.0) 1.0))
+
+allsides = [-1.0,1.0]
+
+seeds0 = circlepairs2seeds [circlepair0] allsides
+
 --- adj circle given radius and angle
 --- TODO: add edge cases (cosv not in [-1.0,1.0] or denom == 0.0)
-circles2circle :: Circle -> Circle -> Float -> Float -> Circle
-circles2circle (Circle c1 r1) (Circle c2 r2) radius side = Circle newcenter radius
+circles2circle :: (Circle,Circle) -> Float -> Float -> Circle
+circles2circle ((Circle c1 r1),(Circle c2 r2)) radius side = Circle newcenter radius
 	       where
 	            newcenter = padd c2 vnew
 		    vnew = vscale (vnorm (vrotate s3 angle) ) l2
@@ -17,5 +29,14 @@ circles2circle (Circle c1 r1) (Circle c2 r2) radius side = Circle newcenter radi
 		    cosv  = (l3 * l3 - l1 * l1 + l2 * l2) / denom
 		    angle = acos( cosv ) * side
 
+
+--- simple packing
+circlepacking :: [Seed] -> Integer -> [Circle]
+circlepacking [] _ = []
+circlepacking _  0 = []
+circlepacking ((Seed (c0,c1) side):xseeds) niter = [newc] ++ (circlepacking (xseeds ++ newseeds) (niter - 1))
+	      where
+	      	  newseeds = circlepairs2seeds [(c0,newc),(c1,newc)] allsides
+	          newc     = circles2circle (c0,c1) (cradius c0) side
 
 
--- test_circlepacking.hs
+++ test_circlepacking.hs
@@ -3,4 +3,5 @@ module Test_CirclePacking where
 import CirclePacking
 import Geoutils
 
-test_circles2circle = circles2circle (Circle (Point 0.0 0.0) 1.0) (Circle (Point 2.0 0.0) 1.0) 1.0 (-1.0)
+test_circles2circle = circles2circle circlepair0 1.0 (-1.0)
+test_circlepacking  = circlepacking seeds0 2
