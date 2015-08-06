5d57b25df764bda5861ce95dd9225e4885de1d8f
JulienLeonard <jl.julien.leonard@gmail.com>
2015-08-06 10:36:25 +1000

    Add simple collision detection for circlepacking

--- circlepacking.hs
+++ circlepacking.hs
@@ -14,6 +14,11 @@ allsides = [-1.0,1.0]
 
 seeds0 = circlepairs2seeds [circlepair0] allsides
 
+circlesfromseeds :: [Seed] -> [Circle]
+circlesfromseeds [] = []
+circlesfromseeds ((Seed (c1,c2) _ ):seeds) = [c1,c2] ++ (circlesfromseeds seeds)
+
+
 --- adj circle given radius and angle
 --- TODO: add edge cases (cosv not in [-1.0,1.0] or denom == 0.0)
 circles2circle :: (Circle,Circle) -> Float -> Float -> Circle
@@ -29,14 +34,25 @@ circles2circle ((Circle c1 r1),(Circle c2 r2)) radius side = Circle newcenter ra
 		    cosv  = (l3 * l3 - l1 * l1 + l2 * l2) / denom
 		    angle = acos( cosv ) * side
 
+--- circlecollidings:
+circlecollidings :: [Circle] -> Circle -> Bool
+circlecollidings [] _ = False
+circlecollidings (c:cs) newc = (cintersects c newc) && (circlecollidings cs newc)
+
+--- trimcollidings: 
+trimcollidings :: [Circle] -> [Circle] -> [Circle]
+trimcollidings _ [] = []
+trimcollidings crefs (c:cs) = newcs ++ (trimcollidings (newcs ++ crefs) cs)
+	       where 
+	           newcs = if (circlecollidings crefs c) then [] else [c]
 
 --- simple packing
-circlepacking :: [Seed] -> Integer -> [Circle]
-circlepacking [] _ = []
-circlepacking _  0 = []
-circlepacking ((Seed (c0,c1) side):xseeds) niter = [newc] ++ (circlepacking (xseeds ++ newseeds) (niter - 1))
+circlepacking :: [Circle] -> [Seed] -> Integer -> [Circle]
+circlepacking _ [] _ = []
+circlepacking _ _  0 = []
+circlepacking cs ((Seed (c0,c1) side):xseeds) niter = newcs ++ (circlepacking (cs ++ newcs) (xseeds ++ newseeds) (niter - 1))
 	      where
-	      	  newseeds = circlepairs2seeds [(c0,newc),(c1,newc)] allsides
-	          newc     = circles2circle (c0,c1) (cradius c0) side
+	      	  newseeds = circlepairs2seeds (concat [ [(c0,newc),(c1,newc)] | newc <- newcs]) allsides
+	          newcs    = trimcollidings cs [circles2circle (c0,c1) (cradius c0) side] 
 
 
