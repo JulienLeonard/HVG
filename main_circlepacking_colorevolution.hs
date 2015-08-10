d352e6541829c930e1a7f4a1ea36c1f5cac69059
JulienLeonard <jl.julien.leonard@gmail.com>
2015-08-11 09:31:38 +1000

    Refactor hue example to use generalized hue paylod for CircleNode

--- main_circlepacking_colorevolution.hs
+++ main_circlepacking_colorevolution.hs
@@ -9,11 +9,17 @@ import CirclePacking
 import Collider
 import System.Random
 
+fnewhue :: Float -> Float -> Float
+fnewhue hue1 hue2 =  0.1 + (max hue1 hue2)
+
+nodehue :: CircleNode Float -> Float
+nodehue node = nodecontent node
+
+
 main = do
      writeFile "circlepacking_colorand.svg" $ svgCircleColors circlecolors
      where 
-         circlecolors  = [((nodecircle node),(hsla2rgba (HSLA hue 1.0 0.5 1.0))) | (node,hue) <- newnodehues]
-	 newnodehues   = zip newnodes randhues
-	 randhues      = take (length newnodes) $ (randomRs (rangevalues range0) (mkStdGen 0))
-	 newnodes      = seed0nodes ++ (circlepacking (Collider seed0nodes) seeds0 (RatioRadius 0.9) 5000)
-	 seed0nodes    = circlenodesfromseeds seeds0
+         circlecolors  = [((nodecircle node),(hsla2rgba (HSLA (nodehue node) 1.0 0.5 1.0))) | node <- newnodes]
+	 newnodes      = seed0nodes ++ (circlepacking (Collider seed0nodes) hueseeds0 (RatioRadius 0.9) fnewhue 5000)
+	 seed0nodes    = circlenodesfromseeds hueseeds0
+	 hueseeds0     = (seeds0 0.0)
